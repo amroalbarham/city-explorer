aec9d44260e8c34c8bb1ab2b122fa38042c4056b
amroalbarham <amroalbarham@gmail.com>
2021-05-25 13:05:43 +0300

    finish lab07

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Informationform from './components/formsinf';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cardsinfo from './components/cardsinfo';
+import WeatherCard from './components/weather';
 
 
 class App extends React.Component {
@@ -9,35 +10,47 @@ class App extends React.Component {
     super(props);
     this.state = {
       data2: '',
-      display:false,
-      show:false,
+      display: false,
+      show: false,
+      weatherArr: [],
+      rend: false,
     }
   }
-  setData = (data1,showing) => {
+  setData = (data1, showing) => {
     this.setState({
       data2: data1,
-      display:showing,
-      show:true,
+      display: showing,
+      show: true,
     })
-    console.log(this.state.data2);
+  }
+
+  setWeatherArr = (weatherData, showing) => {
+    this.setState({
+      weatherArr: weatherData,
+      rend: showing,
+    })
   }
 
 
   render() {
     return (
       <div>
-        <Informationform setData={this.setData} />
-      {this.state.show &&
-        <Cardsinfo data2={this.state.data2} display={this.state.display} />
-      }
+        <Informationform setData={this.setData} setWeather={this.setWeatherArr} />
+        {this.state.show &&
+          <Cardsinfo data2={this.state.data2} display={this.state.display} />
+        }
+        {this.state.show === true &&
+          <WeatherCard display={this.state.rend} weatherData={this.state.weatherArr} />}
+
 
 
       </div>
     )
   }
 }
+
 export default App;
 
-        
-      
-        
+
+
+
